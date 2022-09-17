d1a9492e9635766ce34f37957302e7c5c8f1197a
DaveJabe <davidjabech@ufl.edu>
2022-09-17 12:27:51 -0400

    Initial Commit

---
+++ Shared/LandmarksMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksMockApp.swift
+//  Shared
+//
+//  Created by David Jabech on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
